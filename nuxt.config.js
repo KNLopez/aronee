8fab384bbb1b30bef75a6477d8d2c95f294563cd
Kurt Lopez <kurtdevph@gmail.com>
2019-01-14 12:30:36 +0800

    animations

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,8 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    script: [{ src: '/animations.min.js ', body: true }]
   },
 
   /*
@@ -24,7 +25,7 @@ module.exports = {
   /*
   ** Global CSS
   */
-  css: [],
+  css: ['@/assets/css/main.css'],
 
   /*
   ** Plugins to load before mounting the App
